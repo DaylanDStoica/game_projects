0d9f547018c724d33702639b81c0771d3326b388
DaylanDStoica <daylanstoica@gmail.com>
2023-07-06 16:05:04 -0700

    first commit

---
+++ character_sheets/README.md
@@ -0,0 +1,5 @@
+
+character sheets 
+
+read_sheet: read a text file, and interpret the contents for computations
+
---
+++ character_sheets/read_sheet.py
@@ -0,0 +1,5 @@
+
+# read a character sheet text-file
+# and parse the data into a format that program's can use 
+# read line-by-line 
+
