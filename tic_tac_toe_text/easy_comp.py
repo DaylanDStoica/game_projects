6c20ec8db303f44eb8c19b5dddeb171ceaefc7e5
DaylanDStoica <daylanstoica@gmail.com>
2023-07-06 22:11:03 -0700

    made the files for computer difficulty

---
+++ tic_tac_toe_text/easy_comp.py
@@ -0,0 +1,4 @@
+def comp_decide(the_board):
+    # given a 3x3 tic tac toe board 
+    # randomly choose an open space to place a piece
+    pass 
---
+++ tic_tac_toe_text/hard_comp.py
@@ -0,0 +1,5 @@
+def comp_decide (the_board):
+    # use some decision making
+    # 1. placement of comp's pieces to win
+    # 2. placement of opponent's pieces to prevent their loss
+    pass
