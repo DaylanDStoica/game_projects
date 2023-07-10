e626307a21fd217854a4befcfd62b1edfe6c53c6
DaylanDStoica <daylanstoica@gmail.com>
2023-07-09 22:10:15 -0700

    established header import. Bug: when the computer decides a spot to fill, instead fills up the entire column

--- tic_tac_toe_text/easy_comp.py
+++ tic_tac_toe_text/easy_comp.py
@@ -1,13 +1,28 @@
-from random import randint 
+from random import randint
+from tabnanny import check
+from tkinter import Y 
 # from tic_tac_toe import empty_spot, player1, player2
 # from header import empty_spot, player1, player2
-import header as h
+# import header as h
+# from . import my_header as h
+import my_header as h
 def comp_decide(the_board):
     # given a 3x3 tic tac toe board 
     # randomly choose an open space to place a piece
-    pass 
+    not_yet_placed = True 
+    # as long as an empty spot is not yet found, continue generating coordinates 
+    while not_yet_placed: 
+        y_guess = randint(1,3)
+        x_guess = randint(1,3)
+        print("computer checking spot: (", x_guess, ",", y_guess, ")...")
+        check_result = check_space_for_empty(the_board, x_guess= x_guess-1, y_guess= y_guess-1)
+        if check_result: # the generated coords contain an empty spot 
+            the_board[y_guess-1][x_guess-1] = h.player2
+            # print(the_board)
+            not_yet_placed = False  # a spot has now been placed.
+            break
 
-def check_space_for_empty ( the_board, x_guess, y_guess):
+def check_space_for_empty ( the_board, x_guess=0, y_guess=0):
     # return True if the given coords are empty
     # return False otherwise
     chosen_spot = the_board[y_guess][x_guess]
@@ -15,7 +30,10 @@ def check_space_for_empty ( the_board, x_guess, y_guess):
     #     return False 
     # else: 
     #     return True 
-    if chosen_spot == h.empty_spot:
+    if chosen_spot == h.empty_spot: # the given spot is empty
+        print("   spot ( %1d, %1d) is an empty spot" % (x_guess, y_guess) )
+        # print( chosen_spot)
         return True 
+
     else:
         return False
