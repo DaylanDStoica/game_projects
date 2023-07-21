3c6c07300e791d60847de232f6700b7fa2ac0b06
DaylanDStoica <daylanstoica@gmail.com>
2023-07-21 15:50:49 -0700

    made function for reading from the command line

--- character_sheets/read_sheet.py
+++ character_sheets/read_sheet.py
@@ -3,3 +3,20 @@
 # and parse the data into a format that program's can use 
 # read line-by-line 
 
+def read_file(filename):
+    # parse the contents of the file
+    pass 
+
+
+import sys
+def main():
+    # from the terminal , take value from the line
+    if len(sys.argv) < 2 : 
+        # not enough command line arguments
+        filename = input(" name of file to read from?: ")
+    else: # the file name is on the command line
+        filename = sys.argv[1]
+    # print("length of argv: ", len(sys.argv))
+    read_file(filename)
+
+main()
